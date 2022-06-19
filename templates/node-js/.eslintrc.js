dbe42d3b198d25321ec1a9dccbbfcd5e412e69db
fumagallilaura <la_12_msn@hotmail.com>
2022-06-18 23:25:55 -0300

    chore: eslint config

--- templates/node-js/.eslintrc.js
+++ templates/node-js/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "rules": {
-        "linebreak-style": 0
-    }
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'linebreak-style': 0,
+  },
 };
