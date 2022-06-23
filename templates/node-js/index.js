ed6b5a6b4e9beb12ce4387ae79dc93ce4b138456
Veidz <joaovictorveidz@gmail.com>
2022-06-23 18:37:44 -0300

    feat: add helmet to index.js

--- templates/node-js/index.js
+++ templates/node-js/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet())
 app.use(cors());
 
 app.get('/hello', (req, res) => {
@@ -13,3 +15,4 @@ app.get('/hello', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+ 
