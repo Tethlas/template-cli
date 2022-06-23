22053e057a7433f073782ece7afe9e3ab7764dc7
Veidz <joaovictorveidz@gmail.com>
2022-06-23 18:52:16 -0300

    feat: create base index.js inquirer

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES,
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name: ',
+    validate: (input) => {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      return 'Project name may only includes letter, numbers, underscores and hashes.';
+    }
+  }
+]
+
+inquirer.prompt(QUESTIONS).then(answers => console.log(answers));
